69ce8a21a2ee50bc286e31bda071b2cf553953f8
Shawn Hung <shawnhung0714@gmail.com>
2019-01-04 22:59:04 +0800

    Update config.

--- samples/IVD/IVD.py
+++ samples/IVD/IVD.py
@@ -1,3 +1,21 @@
+import sys
+import os
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+print(f"{ROOT_DIR}")
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+
+from mrcnn import visualize
+from mrcnn import model as modellib
+from mrcnn import utils
+from mrcnn.config import Config
+from mrcnn.model import log
+import skimage.io
+import numpy as np
+import random
+import matplotlib.pyplot as plt
 """
 Mask R-CNN
 Train on the nuclei segmentation dataset from the
@@ -25,40 +43,12 @@ Usage: import the module (see Jupyter notebooks for examples), or run from
     python3 nucleus.py detect --dataset=/path/to/dataset --subset=train --weights=<last or /path/to/weights.h5>
 """
 
-# Set matplotlib backend
-# This has to be done before other importa that might
-# set it, but only if we're running in script mode
-# rather than being imported.
-if __name__ == '__main__':
-    import matplotlib
-    # Agg backend runs without a display
-    matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-
-import os
-import sys
-import json
-import datetime
-import numpy as np
-import skimage.io
-from imgaug import augmenters as iaa
-
-# Root directory of the project
-ROOT_DIR = os.path.abspath("/Users/shawnhung/Documents/Mask_RCNN")
-
-# Import Mask RCNN
-sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn.config import Config
-from mrcnn import utils
-from mrcnn import model as modellib
-from mrcnn import visualize
-
 # Path to trained weights file
 COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
 # Directory to save logs and model checkpoints, if not provided
 # through the command line argument --logs
-DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Results directory
 # Save submission files here
@@ -67,101 +57,35 @@ RESULTS_DIR = os.path.join(ROOT_DIR, "results/IVD/")
 # The dataset doesn't have a standard train/val split, so I picked
 # a variety of images to surve as a validation set.
 VAL_IMAGE_IDS = [
+    "0080",
+    "0081",
+    "0082",
+    "0083",
+    "0084",
+    "0085",
+    "0086",
+    "0087",
+    "0088",
+    "0089",
+    "0090",
+    "0091",
+    "0092",
+    "0093",
+    "0094",
+    "0095",
     "0096",
     "0097",
     "0098",
     "0099"
 ]
 
-
-############################################################
-#  Configurations
-############################################################
-
-class IVDConfig(Config):
-    """Configuration for training on the nucleus segmentation dataset."""
-    # Give the configuration a recognizable name
-    NAME = "IVD"
-
-    # Adjust depending on your GPU memory
-    IMAGES_PER_GPU = 2
-
-    # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  # Background + nucleus
-
-    # Number of training and validation steps per epoch
-    STEPS_PER_EPOCH = (657 - len(VAL_IMAGE_IDS)) // IMAGES_PER_GPU
-    VALIDATION_STEPS = max(1, len(VAL_IMAGE_IDS) // IMAGES_PER_GPU)
-
-    # Don't exclude based on confidence. Since we have two classes
-    # then 0.5 is the minimum anyway as it picks between nucleus and BG
-    DETECTION_MIN_CONFIDENCE = 0
-
-    # Backbone network architecture
-    # Supported values are: resnet50, resnet101
-    BACKBONE = "resnet50"
-
-    # Input image resizing
-    # Random crops of size 512x512
-    IMAGE_RESIZE_MODE = "crop"
-    IMAGE_MIN_DIM = 512
-    IMAGE_MAX_DIM = 512
-    IMAGE_MIN_SCALE = 2.0
-
-    # Length of square anchor side in pixels
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
-
-    # ROIs kept after non-maximum supression (training and inference)
-    POST_NMS_ROIS_TRAINING = 1000
-    POST_NMS_ROIS_INFERENCE = 2000
-
-    # Non-max suppression threshold to filter RPN proposals.
-    # You can increase this during training to generate more propsals.
-    RPN_NMS_THRESHOLD = 0.9
-
-    # How many anchors per image to use for RPN training
-    RPN_TRAIN_ANCHORS_PER_IMAGE = 64
-
-    # Image mean (RGB)
-    MEAN_PIXEL = np.array([43.53, 39.56, 48.22])
-
-    # If enabled, resizes instance masks to a smaller size to reduce
-    # memory load. Recommended when using high-resolution images.
-    USE_MINI_MASK = True
-    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
-
-    # Number of ROIs per image to feed to classifier/mask heads
-    # The Mask RCNN paper uses 512 but often the RPN doesn't generate
-    # enough positive proposals to fill this and keep a positive:negative
-    # ratio of 1:3. You can increase the number of proposals by adjusting
-    # the RPN NMS threshold.
-    TRAIN_ROIS_PER_IMAGE = 128
-
-    # Maximum number of ground truth instances to use in one image
-    MAX_GT_INSTANCES = 200
-
-    # Max number of final detections per image
-    DETECTION_MAX_INSTANCES = 400
-
-
-class IVDInferenceConfig(IVDConfig):
-    # Set batch size to 1 to run one image at a time
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-    # Don't resize imager for inferencing
-    IMAGE_RESIZE_MODE = "pad64"
-    # Non-max suppression threshold to filter RPN proposals.
-    # You can increase this during training to generate more propsals.
-    RPN_NMS_THRESHOLD = 0.7
-
-
 ############################################################
 #  Dataset
 ############################################################
 
-class IVDDataset(utils.Dataset):
 
-    def load_IVD(self, dataset_dir, subset):
+class IVDDataset(utils.Dataset):
+    def load_IVD(self, subset):
         """Load a subset of the nuclei dataset.
 
         dataset_dir: Root directory of the dataset
@@ -173,30 +97,30 @@ class IVDDataset(utils.Dataset):
         # Add classes. We have one class.
         # Naming the dataset nucleus, and the class nucleus
         self.add_class("IVD", 1, "IVD")
-        self.dataset_dir = dataset_dir
+        self.dataset_dir = os.path.join(ROOT_DIR, 'datasets/IVD')
 
         # Which subset?
         # "val": use hard-coded list above
         # "train": use data from stage1_train minus the hard-coded list above
         # else: use the data from the specified sub-directory
 
-        assert subset in ["train", "val", "stage1_train", "stage1_test", "stage2_test"]
+        assert subset in ["train", "val"]
+
         # subset_dir = "stage1_train" if subset in ["train", "val"] else subset
         # dataset_dir = os.path.join(dataset_dir, subset_dir)
+        image_ids = next(os.walk(self.dataset_dir))[1]
         if subset == "val":
             image_ids = VAL_IMAGE_IDS
         else:
             # Get image ids from directory names
-            image_ids = next(os.walk(dataset_dir))[1]
-            if subset == "train":
-                image_ids = list(set(image_ids) - set(VAL_IMAGE_IDS))
+            image_ids = list(set(image_ids) - set(VAL_IMAGE_IDS))
 
         # Add images
         for image_id in image_ids:
             self.add_image(
                 "IVD",
                 image_id=image_id,
-                path=os.path.join(dataset_dir, image_id, "input.png"))
+                path=os.path.join(self.dataset_dir, image_id, "input.png"))
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -212,7 +136,8 @@ class IVDDataset(utils.Dataset):
         mask = []
         for f in next(os.walk(mask_dir))[2]:
             if f.startswith('mask_') and f.endswith(".png"):
-                m = skimage.io.imread(os.path.join(mask_dir, f)).astype(np.bool)
+                m = skimage.io.imread(os.path.join(
+                    mask_dir, f)).astype(np.bool)
                 mask.append(m)
         mask = np.stack(mask, axis=-1)
         # Return mask, and array of class IDs of each instance. Since we have
@@ -228,212 +153,162 @@ class IVDDataset(utils.Dataset):
             super(self.__class__, self).image_reference(image_id)
 
 
+dataset_train = IVDDataset()
+dataset_train.load_IVD('train')
+dataset_train.prepare()
+
+dataset_val = IVDDataset()
+dataset_val.load_IVD("val")
+dataset_val.prepare()
+
+############################################################
+#  Configuration
+############################################################
+
+
+class IVDConfig(Config):
+    """Configuration for training on the nucleus segmentation dataset."""
+    # Give the configuration a recognizable name
+    NAME = "IVD"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Uncomment to train on 8 GPUs (default is 1)
+    # GPU_COUNT = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # background + vertebral
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 10
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+
+class IVDInferenceConfig(IVDConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
 ############################################################
 #  Training
 ############################################################
 
-def train(model, dataset_dir, subset):
+
+def train(model):
     """Train the model."""
-    # Training dataset.
-    dataset_train = IVDDataset()
-    dataset_train.load_IVD(dataset_dir, subset)
-    dataset_train.prepare()
 
-    # Validation dataset
-    dataset_val = IVDDataset()
-    dataset_val.load_IVD(dataset_dir, "val")
-    dataset_val.prepare()
-
-    # Image augmentation
-    # http://imgaug.readthedocs.io/en/latest/source/augmenters.html
-    augmentation = iaa.SomeOf((0, 2), [
-        iaa.Fliplr(0.5),
-        iaa.Flipud(0.5),
-        iaa.OneOf([iaa.Affine(rotate=90),
-                   iaa.Affine(rotate=180),
-                   iaa.Affine(rotate=270)]),
-        iaa.Multiply((0.8, 1.5)),
-        iaa.GaussianBlur(sigma=(0.0, 5.0))
-    ])
-
-    # *** This training schedule is an example. Update to your needs ***
-
-    # If starting from imagenet, train heads only for a bit
-    # since they have random weights
-    print("Train network heads")
+    # Train the head branches
+    # Passing layers="heads" freezes all layers except the head
+    # layers. You can also pass a regular expression to select
+    # which layers to train by name pattern.
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=20,
-                augmentation=augmentation,
+                epochs=30,
                 layers='heads')
-
-    print("Train all layers")
-    model.train(dataset_train, dataset_val,
-                learning_rate=config.LEARNING_RATE,
-                epochs=40,
-                augmentation=augmentation,
-                layers='all')
-
-
-############################################################
-#  RLE Encoding
-############################################################
-
-def rle_encode(mask):
-    """Encodes a mask in Run Length Encoding (RLE).
-    Returns a string of space-separated values.
-    """
-    assert mask.ndim == 2, "Mask must be of shape [Height, Width]"
-    # Flatten it column wise
-    m = mask.T.flatten()
-    # Compute gradient. Equals 1 or -1 at transition points
-    g = np.diff(np.concatenate([[0], m, [0]]), n=1)
-    # 1-based indicies of transition points (where gradient != 0)
-    rle = np.where(g != 0)[0].reshape([-1, 2]) + 1
-    # Convert second index in each pair to lenth
-    rle[:, 1] = rle[:, 1] - rle[:, 0]
-    return " ".join(map(str, rle.flatten()))
-
-
-def rle_decode(rle, shape):
-    """Decodes an RLE encoded list of space separated
-    numbers and returns a binary mask."""
-    rle = list(map(int, rle.split()))
-    rle = np.array(rle, dtype=np.int32).reshape([-1, 2])
-    rle[:, 1] += rle[:, 0]
-    rle -= 1
-    mask = np.zeros([shape[0] * shape[1]], np.bool)
-    for s, e in rle:
-        assert 0 <= s < mask.shape[0]
-        assert 1 <= e <= mask.shape[0], "shape: {}  s {}  e {}".format(shape, s, e)
-        mask[s:e] = 1
-    # Reshape and transpose
-    mask = mask.reshape([shape[1], shape[0]]).T
-    return mask
-
-
-def mask_to_rle(image_id, mask, scores):
-    "Encodes instance masks to submission format."
-    assert mask.ndim == 3, "Mask must be [H, W, count]"
-    # If mask is empty, return line with image ID only
-    if mask.shape[-1] == 0:
-        return "{},".format(image_id)
-    # Remove mask overlaps
-    # Multiply each instance mask by its score order
-    # then take the maximum across the last dimension
-    order = np.argsort(scores)[::-1] + 1  # 1-based descending
-    mask = np.max(mask * np.reshape(order, [1, 1, -1]), -1)
-    # Loop over instance masks
-    lines = []
-    for o in order:
-        m = np.where(mask == o, 1, 0)
-        # Skip if empty
-        if m.sum() == 0.0:
-            continue
-        rle = rle_encode(m)
-        lines.append("{}, {}".format(image_id, rle))
-    return "\n".join(lines)
-
-
 ############################################################
 #  Detection
 ############################################################
 
-def detect(model, dataset_dir, subset):
+
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
+
+def detect(model):
     """Run detection on images in the given directory."""
-    print("Running on {}".format(dataset_dir))
 
-    # Create directory
-    if not os.path.exists(RESULTS_DIR):
-        os.makedirs(RESULTS_DIR)
-    submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
-    submit_dir = os.path.join(RESULTS_DIR, submit_dir)
-    os.makedirs(submit_dir)
+    # Recreate the model in inference mode
+    # Get path to saved weights
+    # Either set a specific path or find last trained weights
+    # model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+    model_path = model.find_last()
 
-    # Read dataset
-    dataset = IVDDataset()
-    dataset.load_IDV(dataset_dir, subset)
-    dataset.prepare()
-    # Load over images
-    submission = []
-    for image_id in dataset.image_ids:
-        # Load image and run detection
-        image = dataset.load_image(image_id)
-        # Detect objects
-        r = model.detect([image], verbose=0)[0]
-        # Encode image to RLE. Returns a string of multiple lines
-        source_id = dataset.image_info[image_id]["id"]
-        rle = mask_to_rle(source_id, r["masks"], r["scores"])
-        submission.append(rle)
-        # Save image with masks
-        visualize.display_instances(
-            image, r['rois'], r['masks'], r['class_ids'],
-            dataset.class_names, r['scores'],
-            show_bbox=False, show_mask=False,
-            title="Predictions")
-        plt.savefig("{}/{}.png".format(submit_dir, dataset.image_info[image_id]["id"]))
+    # Load trained weights
+    print("Loading weights from ", model_path)
+    model.load_weights(model_path, by_name=True)
 
-    # Save to csv file
-    submission = "ImageId,EncodedPixels\n" + "\n".join(submission)
-    file_path = os.path.join(submit_dir, "submit.csv")
-    with open(file_path, "w") as f:
-        f.write(submission)
-    print("Saved to ", submit_dir)
+    dataset_val = IVDDataset()
+    dataset_val.load_IVD("val")
+    dataset_val.prepare()
+
+    # Test on a random image
+    image_id = random.choice(dataset_val.image_ids)
+    original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+        modellib.load_image_gt(dataset_val, inference_config,
+                               image_id, use_mini_mask=False)
+
+    info = dataset_val.image_info[image_id]
+    log(f'image ID: {info["source"]}.{ info["id"]} \
+        ({image_id}){dataset_val.image_reference(image_id)}')
+    log("original_image", original_image)
+    log("image_meta", image_meta)
+    log("gt_class_id", gt_class_id)
+    log("gt_bbox", gt_bbox)
+    log("gt_mask", gt_mask)
+
+    visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
+                                dataset_train.class_names, figsize=(8, 8))
+    results = model.detect([original_image], verbose=1)
+    r = results[0]
+    visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'],
+                                dataset_val.class_names, r['scores'], ax=get_ax())
 
 
 ############################################################
 #  Command Line
 ############################################################
-
 if __name__ == '__main__':
-
     import argparse
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(
-        description='Mask R-CNN for nuclei counting and segmentation')
+        description='Train Mask R-CNN to detect vertebrals.')
     parser.add_argument("command",
                         metavar="<command>",
-                        help="'train' or 'detect'")
-    parser.add_argument('--dataset', required=False,
-                        metavar="/path/to/dataset/",
-                        help='Root directory of the dataset')
+                        help="'train' or 'test'")
     parser.add_argument('--weights', required=True,
                         metavar="/path/to/weights.h5",
                         help="Path to weights .h5 file or 'coco'")
     parser.add_argument('--logs', required=False,
-                        default=DEFAULT_LOGS_DIR,
+                        default=MODEL_DIR,
                         metavar="/path/to/logs/",
                         help='Logs and checkpoints directory (default=logs/)')
-    parser.add_argument('--subset', required=False,
-                        metavar="Dataset sub-directory",
-                        help="Subset of dataset to run prediction on")
+    parser.add_argument('--image', required=False,
+                        metavar="path to image",
+                        help='Image to apply the color splash effect on')
     args = parser.parse_args()
 
-    # Validate arguments
-    if args.command == "train":
-        assert args.dataset, "Argument --dataset is required for training"
-    elif args.command == "detect":
-        assert args.subset, "Provide --subset to run prediction on"
-
     print("Weights: ", args.weights)
-    print("Dataset: ", args.dataset)
-
-    if args.subset:
-        print("Subset: ", args.subset)
     print("Logs: ", args.logs)
 
     # Configurations
-    config = IVDConfig() if args.command == "train" else IVDInferenceConfig()
+    if args.command == "train":
+        config = IVDConfig()
+    else:
+        config = IVDInferenceConfig()
     config.display()
 
     # Create model
     if args.command == "train":
-        model = modellib.MaskRCNN(mode="training", config=config,
-                                  model_dir=args.logs)
+        model = modellib.MaskRCNN(mode="training",
+                                  config=config,
+                                  model_dir=MODEL_DIR)
     else:
-        model = modellib.MaskRCNN(mode="inference", config=config,
-                                  model_dir=args.logs)
+        inference_config = IVDInferenceConfig()
+        model = modellib.MaskRCNN(mode="inference",
+                                  config=inference_config,
+                                  model_dir=MODEL_DIR)
 
     # Select weights file to load
     if args.weights.lower() == "coco":
@@ -463,9 +338,9 @@ if __name__ == '__main__':
 
     # Train or evaluate
     if args.command == "train":
-        train(model, args.dataset, args.subset)
+        train(model)
     elif args.command == "detect":
-        detect(model, args.dataset, args.subset)
+        detect(model)
     else:
         print("'{}' is not recognized. "
-              "Use 'train' or 'detect'".format(args.command))
+              "Use 'train' or 'splash'".format(args.command))
